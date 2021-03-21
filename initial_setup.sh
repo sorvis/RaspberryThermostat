67969c7667e188ee2d9944ba27869b958a3ab1e6
Steven Orvis <steven@huntingpa.info>
2021-03-21 09:00:58 -0400

    Add -y for auto yes

--- initial_setup.sh
+++ initial_setup.sh
@@ -19,7 +19,7 @@ sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 0x219BD9C
 wget -qO - 'https://bintray.com/user/downloadSubjectPublicKey?username=openhab' | sudo apt-key add - \
 && echo 'deb https://dl.bintray.com/openhab/apt-repo2 stable main' | sudo tee /etc/apt/sources.list.d/openhab2.list \
 && sudo apt-get update \
-&& sudo apt-get install openhab2 \
+&& sudo apt-get install -y openhab2 \
 && sudo apt-get autoremove -y \
 && sudo /bin/systemctl daemon-reload \
 && sudo /bin/systemctl enable openhab2.service \
